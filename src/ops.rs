9beb0eed8d57fde9a5f4491317c7fcf5528458e4
Paul Loyd <pavelko95@gmail.com>
2020-12-03 03:24:58 +0300

    feat(ops): impl ops for primitive types

--- src/ops.rs
+++ src/ops.rs
@@ -1,3 +1,5 @@
+use crate::ArithmeticError;
+
 pub trait Numeric: Copy {
     const ZERO: Self;
     const ONE: Self;
@@ -51,4 +53,83 @@ pub trait RoundingDiv<Rhs = Self> {
     type Error;
 
     fn rdiv(self, rhs: Rhs, mode: RoundMode) -> Result<Self::Output, Self::Error>;
+}
+
+// Impls for primitives.
+
+macro_rules! impl_for_ints {
+    ($int:ty, $($rest:ty),*) => {
+        impl_for_ints!($int);
+        impl_for_ints!($($rest),*);
+    };
+    ($int:ty) => {
+        impl CheckedAdd for $int {
+            type Output = $int;
+            type Error = ArithmeticError;
+
+            #[inline]
+            fn cadd(self, rhs: Self) -> Result<Self::Output, Self::Error> {
+                self.checked_add(rhs).ok_or(ArithmeticError::Overflow)
+            }
+        }
+
+        impl CheckedSub for $int {
+            type Output = $int;
+            type Error = ArithmeticError;
+
+            #[inline]
+            fn csub(self, rhs: Self) -> Result<Self::Output, Self::Error> {
+                self.checked_sub(rhs).ok_or(ArithmeticError::Overflow)
+            }
+        }
+
+        impl CheckedMul for $int {
+            type Output = $int;
+            type Error = ArithmeticError;
+
+            #[inline]
+            fn cmul(self, rhs: Self) -> Result<Self::Output, Self::Error> {
+                self.checked_mul(rhs).ok_or(ArithmeticError::Overflow)
+            }
+        }
+
+        impl RoundingDiv for $int {
+            type Output = $int;
+            type Error = ArithmeticError;
+
+            #[inline]
+            fn rdiv(self, rhs: Self, mode: RoundMode) -> Result<Self::Output, Self::Error> {
+                if rhs == 0 {
+                    return Err(ArithmeticError::DivisionByZero);
+                }
+
+                let mut result = self / rhs;
+                let loss = self - result * rhs;
+
+                if loss != 0 {
+                    let sign = self.signum() * rhs.signum();
+
+                    if mode as i32 == sign as i32 {
+                        result = result.checked_add(sign).ok_or(ArithmeticError::Overflow)?;
+                    }
+                }
+
+                Ok(result)
+            }
+        }
+    };
+}
+
+impl_for_ints!(i8, i16, i32, i64, i128); // TODO: unsigned?
+
+#[test]
+fn it_rounds_rdiv() {
+    assert_eq!(5.rdiv(2, RoundMode::Floor), Ok(2));
+    assert_eq!(5.rdiv(2, RoundMode::Ceil), Ok(3));
+    assert_eq!((-5).rdiv(2, RoundMode::Floor), Ok(-3));
+    assert_eq!((-5).rdiv(2, RoundMode::Ceil), Ok(-2));
+    assert_eq!(5.rdiv(-2, RoundMode::Floor), Ok(-3));
+    assert_eq!(5.rdiv(-2, RoundMode::Ceil), Ok(-2));
+    assert_eq!((-5).rdiv(-2, RoundMode::Floor), Ok(2));
+    assert_eq!((-5).rdiv(-2, RoundMode::Ceil), Ok(3));
 }
