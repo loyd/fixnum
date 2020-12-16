668c6ddbb1ccd130286c2562a3d69a75da5cdda1
Petr Ermishkin <petr@quasiyoke.me>
2020-12-16 15:29:56 +0300

    refactor: expand `impl_error` macro

--- src/errors.rs
+++ src/errors.rs
@@ -1,23 +1,6 @@
-#[cfg(feature = "std")]
+use core::fmt::{Display, Formatter, Result};
+
 use derive_more::Error;
-
-macro_rules! impl_error {
-    ($enum:ty, $($variant:pat => $str:expr, )+) => {
-        impl $enum {
-            pub const fn as_str(&self) -> &'static str {
-                match self {
-                    $( $variant => { $str }, )*
-                }
-            }
-        }
-
-        impl ::core::fmt::Display for $enum {
-            fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
-                f.write_str(self.as_str())
-            }
-        }
-    };
-}
 
 #[cfg_attr(feature = "std", derive(Error))]
 #[derive(Debug, PartialEq)]
@@ -26,10 +9,20 @@ pub enum ArithmeticError {
     DivisionByZero,
 }
 
-impl_error!(ArithmeticError,
-    ArithmeticError::Overflow => "overflow",
-    ArithmeticError::DivisionByZero => "division by zero",
-);
+impl ArithmeticError {
+    pub const fn as_str(&self) -> &'static str {
+        match self {
+            Self::Overflow => "overflow",
+            Self::DivisionByZero => "division by zero",
+        }
+    }
+}
+
+impl Display for ArithmeticError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        f.write_str(self.as_str())
+    }
+}
 
 #[cfg_attr(feature = "std", derive(Error))]
 #[derive(Debug, PartialEq)]
@@ -38,10 +31,20 @@ pub enum FromDecimalError {
     TooBigMantissa,
 }
 
-impl_error!(FromDecimalError,
-    FromDecimalError::UnsupportedExponent => "unsupported exponent",
-    FromDecimalError::TooBigMantissa => "mantissa is too big",
-);
+impl FromDecimalError {
+    pub const fn as_str(&self) -> &'static str {
+        match self {
+            Self::UnsupportedExponent => "unsupported exponent",
+            Self::TooBigMantissa => "mantissa is too big",
+        }
+    }
+}
+
+impl Display for FromDecimalError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        f.write_str(self.as_str())
+    }
+}
 
 #[cfg_attr(feature = "std", derive(Error))]
 #[derive(Debug, PartialEq)]
@@ -50,7 +53,17 @@ pub enum ConvertError {
     Other(#[cfg_attr(feature = "std", error(not(source)))] &'static str),
 }
 
-impl_error!(ConvertError,
-    ConvertError::Overflow => "unsupported exponent",
-    ConvertError::Other(_) => "mantissa is too big",
-);
+impl ConvertError {
+    pub const fn as_str(&self) -> &'static str {
+        match self {
+            Self::Overflow => "unsupported exponent",
+            Self::Other(_) => "mantissa is too big",
+        }
+    }
+}
+
+impl Display for ConvertError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        f.write_str(self.as_str())
+    }
+}
